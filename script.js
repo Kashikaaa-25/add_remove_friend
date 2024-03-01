5e47c8fc2067f597964151cc39dfed5815a369e4
Kashika <kashikagarg25@gmail.com>
2024-03-01 20:13:17 +0530

    change

--- script.js
+++ script.js
@@ -1,39 +1,45 @@
-var textChng = document.querySelector("h5");
-var btnChng = document.querySelector("btn");
-var flag = 0;
-var img = document.querySelector("img");
-var love = document.querySelector("i");
-var area =  document.querySelector("body");
-var cmove = document.querySelector(".cursor");
+// // add friend remove friend
+// var textChng = document.querySelector("h5");
+// var btnChng = document.querySelector("btn");
+// var flag = 0;
 
+// btnChng.addEventListener("click",function(){
+//     if(flag==0){
+//         textChng.innerHTML = "Friends";
+//         textChng.style.color = "green";
+//         btnChng.innerHTML = "Remove";
+//         flag=1;
+//     }
+//     else{
+//         textChng.innerHTML = "Stranger";
+//         textChng.style.color = "red";
+//         btnChng.innerHTML = "Add Friend"
+//         flag=0;
+//     }
 
-btnChng.addEventListener("click",function(){
-    if(flag==0){
-        textChng.innerHTML = "Friends";
-        textChng.style.color = "green";
-        btnChng.innerHTML = "Remove";
-        flag=1;
-    }
-    else{
-        textChng.innerHTML = "Stranger";
-        textChng.style.color = "red";
-        btnChng.innerHTML = "Add Friend"
-        flag=0;
-    }
+// })
 
-})
+// // instagram love react
+// var img = document.querySelector("img");
+// var love = document.querySelector("i");
 
-img.addEventListener("dblclick",function(){
-    love.style.transform = "scale(1)";
-    love.style.opacity = "1";
-    love.style.transition = "all ease 0.5s";
-    setTimeout(function(){
-        love.style.transform = "scale(0)";
-        love.style.transition = "all";
-    },1000)
-})
+// img.addEventListener("dblclick",function(){
+//     love.style.transform = "scale(1)";
+//     love.style.opacity = "1";
+//     love.style.transition = "all ease 0.5s";
+//     setTimeout(function(){
+//         love.style.transform = "scale(0)";
+//         love.style.transition = "all";
+//     },1000)
+// })
 
-area.addEventListener("mousemove",function(dets){
-    cmove.style.left = dets.x+ "px";
-    cmove.style.top = dets.y+"px";
-})
+// // custom cursor
+// var area =  document.querySelector("body");
+// var cmove = document.querySelector(".cursor");
+
+// area.addEventListener("mousemove",function(dets){
+//     cmove.style.left = dets.x + "px";
+//     cmove.style.top = dets.y + "px";
+// })
+
+// // multiple image hovering animation
